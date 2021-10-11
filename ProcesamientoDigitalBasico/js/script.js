6b0b2ee7e86b559d2a981a6b807a9438634c85f3
Alberth-h <jesalbertohyfx@hotmail.com>
2021-10-11 13:34:42 -0700

    canny

--- ProcesamientoDigitalBasico/js/script.js
+++ ProcesamientoDigitalBasico/js/script.js
@@ -17,6 +17,21 @@ const process = () => {
                 cv.threshold(mat, mat_result, value, 255, cv.THRESH_BINARY);
                 break;
             }
+        case 2:
+            {
+                const value = parseInt($("#value_2").val());
+                const ksize = new cv.Size(value, value);
+                cv.GaussianBlur(mat, mat_result, ksize, 0, 0, cv.BORDER_DEFAULT);
+                break;
+            }
+        case 3:
+            {
+                const value1 = parseInt($("#value_3_1").val());
+                const value2 = parseInt($("#value_3_2").val());
+                const value_Aperture = parseInt($("#value_3").val());
+                cv.Canny(mat, mat_result, value1, value2, value_Aperture);
+                break;
+            }
     }
     cv.imshow('canvasOutput', mat_result);
     mat.delete();
@@ -38,10 +53,33 @@ const onOpenCvReady = () => {
         $("#step3_" + filter).show();
     });
 
+    //Threshold
     $("#value").change(function() {
-        $("value_sel").html($(this).val());
+        $("#value_sel").html($(this).val());
         process();
-    });
+    })
+
+    //GaussianBlur
+    $("#value_2").change(function() {
+        $("#value_2_sel").html($(this).val() + "x" + $(this).val());
+        process();
+    })
+
+    //Canny
+    $("#value_3_1").change(function() {
+        $("#value_3_1_sel").html($(this).val());
+        process();
+    })
+
+    $("#value_3_2").change(function() {
+        $("#value_3_2_sel").html($(this).val());
+        process();
+    })
+
+    $("#value_3").change(function() {
+        $("#value_3_sel").html($(this).val());
+        process();
+    })
 }
 
 //Cargo mal
